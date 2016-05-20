0e8ed56f0d7f56ec06eeb78a20ed8619cae5b1a5
omokehine1 <omokehine1>
2016-05-20 19:18:37 +0100

    backup

--- hello.js
+++ hello.js
@@ -1,10 +1,13 @@
+// var value ;
 $(document).ready(function() {
 	$.ajax({
-		url: "https://api.github.com/users/+value"
+		url: "https://api.github.com/users/omokehinde"
 	}).then(function(data) {
 		$('.get-id').append(data.id);
 		$('.get-name').append(data.name);
 		$('.get-login').append(data.login);
 		$('.get-avatar').append(data.avatar_url)
+		// value = data.name;
 	});
+
 });
